6892a33e727a9e2eee3f054c8c93d0818bb189c8
Missy Schultz <creativedarkness@wi.rr.com>
2022-10-20 18:43:50 -0500

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
+// import ReactDOM from 'react-dom'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
-const container = document.getElementById('root')
-const root = createRoot(container)
+const root = createRoot(document.getElementById('root'))
 
 root.render(<App />)
