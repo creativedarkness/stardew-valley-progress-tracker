b354dcaeafd352a5dc8c6dc67ee00c6f983e1c96
Missy Schultz <creativedarkness@wi.rr.com>
2022-10-23 12:34:33 -0500

    Install packages update syntax

--- webpack.config.js
+++ webpack.config.js
@@ -3,29 +3,29 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
-let mode = "development"
+let mode = 'development'
 
-if (process.env.NODE_ENV === "production") mode = "production"
+if (process.env.NODE_ENV === 'production') mode = 'production'
 
 module.exports = {
   mode: mode,
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "bundle.js"
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
   },
   plugins: [
     new CleanWebpackPlugin(), // Removes build folder(s) before building
     new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx'],
     alias: {
-          "@": path.resolve(__dirname, "src"),
-          "@components": path.resolve(__dirname, "src/components")
+          '@': path.resolve(__dirname, 'src'),
+          '@components': path.resolve(__dirname, 'src/components')
         }
   },
   module: {
@@ -35,22 +35,22 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           // without additional settings, this will reference .babelrc
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
       {
         test: /\.(s[ac]|c)ss$/i,
         use: [
           { loader: MiniCssExtractPlugin.loader },
-          "css-loader",
+          'css-loader',
           // according to the docs, sass-loader should be at the bottom, which
           // loads it first to avoid prefixes in your sourcemaps and other issues.
-          "sass-loader",
+          'sass-loader',
         ]
       }
     ]
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   devServer: {
     port: 3000,
     hot: true
